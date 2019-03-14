bee075101449d4ef4edb199a80b14e424280ddcf
lazzben <458861463@qq.com>
2019-03-14 21:01:15 +0800

    first-commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from 'antd';
+import * as React from 'react'
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <Button type="primary" size='small'>Primary</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
